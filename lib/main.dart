63e3b97ebeab94bfafc4683765a0ea67e4b9a4b0
aungthureintun <eivalahan122021@gmail.com>
2022-12-10 15:47:08 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Time Input Picker Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: const ColorScheme.light()),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TimeOfDay _time = TimeOfDay.now();
+
+  void _selectTime() async {
+    final TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: _time,
+      initialEntryMode: TimePickerEntryMode.input,
+    );
+    if (newTime != null) {
+      setState(() {
+        _time = newTime;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _selectTime,
+              child: const Text('SELECT TIME'),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Selected time: ${_time.format(context)}',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
